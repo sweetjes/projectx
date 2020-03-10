0603135dcdd593a513dc010448a6faa62918a69c
sweetjes <37630210+sweetjes@users.noreply.github.com>
2020-03-10 15:54:39 +0300

    commited

--- projectx/src/js/script.js
+++ projectx/src/js/script.js
@@ -1,3 +1,17 @@
 var hallseat = document.querySelectorAll(".content-hallseats-row-seat");
+var bookedSeats = {
+  booked: ["seat-5", "seat-13", "seat-64", "seat-43", "seat-65", "seat-78", "seat-23"]
+}
+
+var selected = document.addEventListener("click", function (event) {
+  if (event.target.classList.contains('content-hallseats-row-seat')) {
+    event.toElement.classList.add('color');
+  }
+
+});
+
+
+
+
 
 
