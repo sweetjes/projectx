ff5c69988db99f8fbf4051ba40700747cc2b8c1f
sweetjes <37630210+sweetjes@users.noreply.github.com>
2020-03-16 23:18:54 +0300

    commitsss

--- projectx/src/js/script.js
+++ projectx/src/js/script.js
@@ -1,17 +1,157 @@
-var hallseat = document.querySelectorAll(".content-hallseats-row-seat");
-var bookedSeats = {
-  booked: ["seat-5", "seat-13", "seat-64", "seat-43", "seat-65", "seat-78", "seat-23"]
-}
-
-var selected = document.addEventListener("click", function (event) {
-  if (event.target.classList.contains('content-hallseats-row-seat')) {
-    event.toElement.classList.add('color');
-  }
-
+var selected = document.addEventListener("click", function(event) {
+    if (event.target.classList.contains("content-hallseats-row-seat")) {
+        event.toElement.classList.add("color");
+        document.querySelector(".buy-button").style.visibility = "visible";
+    }
 });
 
+var bighall = [
+    { seat: 1, row: 1, booked: true },
+    { seat: 2, row: 1, booked: false },
+    { seat: 3, row: 1, booked: false },
+    { seat: 4, row: 1, booked: false },
+    { seat: 5, row: 1, booked: false },
+    { seat: 6, row: 1, booked: false },
+    { seat: 7, row: 1, booked: false },
+    { seat: 8, row: 1, booked: false },
+    { seat: 9, row: 1, booked: true },
+    { seat: 10, row: 1, booked: false },
+    { seat: 11, row: 1, booked: false },
+    { seat: 12, row: 1, booked: false },
+    { seat: 13, row: 1, booked: false },
+    { seat: 14, row: 1, booked: false },
+    { seat: 15, row: 1, booked: false },
 
+    { seat: 1, row: 2, booked: false },
+    { seat: 2, row: 2, booked: false },
+    { seat: 3, row: 2, booked: false },
+    { seat: 4, row: 2, booked: false },
+    { seat: 5, row: 2, booked: false },
+    { seat: 6, row: 2, booked: false },
+    { seat: 7, row: 2, booked: false },
+    { seat: 8, row: 2, booked: false },
+    { seat: 9, row: 2, booked: true },
+    { seat: 10, row: 2, booked: false },
+    { seat: 11, row: 2, booked: false },
+    { seat: 12, row: 2, booked: false },
+    { seat: 13, row: 2, booked: false },
+    { seat: 14, row: 2, booked: false },
+    { seat: 15, row: 2, booked: false },
+    { seat: 16, row: 2, booked: false },
+    { seat: 17, row: 2, booked: false },
 
+    { seat: 1, row: 3, booked: false },
+    { seat: 2, row: 3, booked: false },
+    { seat: 3, row: 3, booked: false },
+    { seat: 4, row: 3, booked: true },
+    { seat: 5, row: 3, booked: false },
+    { seat: 6, row: 3, booked: false },
+    { seat: 7, row: 3, booked: false },
+    { seat: 8, row: 3, booked: false },
+    { seat: 9, row: 3, booked: false },
+    { seat: 10, row: 3, booked: false },
+    { seat: 11, row: 3, booked: false },
+    { seat: 12, row: 3, booked: true },
+    { seat: 13, row: 3, booked: false },
+    { seat: 14, row: 3, booked: false },
+    { seat: 15, row: 3, booked: false },
+    { seat: 16, row: 3, booked: false },
+    { seat: 17, row: 3, booked: false },
 
+    { seat: 1, row: 4, booked: false },
+    { seat: 2, row: 4, booked: false },
+    { seat: 3, row: 4, booked: false },
+    { seat: 4, row: 4, booked: false },
+    { seat: 5, row: 4, booked: false },
+    { seat: 6, row: 4, booked: false },
+    { seat: 7, row: 4, booked: false },
+    { seat: 8, row: 4, booked: false },
+    { seat: 9, row: 4, booked: false },
+    { seat: 10, row: 4, booked: false },
+    { seat: 11, row: 4, booked: false },
+    { seat: 12, row: 4, booked: false },
+    { seat: 13, row: 4, booked: false },
+    { seat: 14, row: 4, booked: false },
+    { seat: 15, row: 4, booked: false },
+    { seat: 16, row: 4, booked: false },
+    { seat: 17, row: 4, booked: false },
 
+    { seat: 1, row: 5, booked: false },
+    { seat: 2, row: 5, booked: false },
+    { seat: 3, row: 5, booked: false },
+    { seat: 4, row: 5, booked: true },
+    { seat: 5, row: 5, booked: false },
+    { seat: 6, row: 5, booked: false },
+    { seat: 7, row: 5, booked: false },
+    { seat: 8, row: 5, booked: false },
+    { seat: 9, row: 5, booked: false },
+    { seat: 10, row: 5, booked: false },
+    { seat: 11, row: 5, booked: true },
+    { seat: 12, row: 5, booked: false },
+    { seat: 13, row: 5, booked: false },
+    { seat: 14, row: 5, booked: false },
+    { seat: 15, row: 5, booked: false },
+    { seat: 16, row: 5, booked: false },
+    { seat: 17, row: 5, booked: false },
+    { seat: 18, row: 5, booked: false },
+    { seat: 19, row: 5, booked: false },
 
+    { seat: 1, row: 6, booked: true },
+    { seat: 2, row: 6, booked: true },
+    { seat: 3, row: 6, booked: false },
+    { seat: 4, row: 6, booked: false },
+    { seat: 5, row: 6, booked: false },
+    { seat: 6, row: 6, booked: false },
+    { seat: 7, row: 6, booked: false },
+    { seat: 8, row: 6, booked: false },
+    { seat: 9, row: 6, booked: false },
+    { seat: 10, row: 6, booked: false },
+    { seat: 11, row: 6, booked: true },
+    { seat: 12, row: 6, booked: false },
+    { seat: 13, row: 6, booked: true },
+    { seat: 14, row: 6, booked: true },
+    { seat: 15, row: 6, booked: false },
+    { seat: 16, row: 6, booked: false },
+    { seat: 17, row: 6, booked: false },
+    { seat: 18, row: 6, booked: false },
+    { seat: 19, row: 6, booked: false },
+
+    { seat: 1, row: 7, booked: false },
+    { seat: 2, row: 7, booked: false },
+    { seat: 3, row: 7, booked: false },
+    { seat: 4, row: 7, booked: true },
+    { seat: 5, row: 7, booked: true },
+    { seat: 6, row: 7, booked: false },
+    { seat: 7, row: 7, booked: false },
+    { seat: 8, row: 7, booked: false },
+    { seat: 9, row: 7, booked: false },
+    { seat: 10, row: 7, booked: false },
+    { seat: 11, row: 7, booked: false },
+    { seat: 12, row: 7, booked: false },
+    { seat: 13, row: 7, booked: false },
+    { seat: 14, row: 7, booked: false },
+    { seat: 15, row: 7, booked: false },
+    { seat: 16, row: 7, booked: true },
+    { seat: 17, row: 7, booked: true },
+    { seat: 18, row: 7, booked: false },
+    { seat: 19, row: 7, booked: false }
+];
+
+let hallseat = document.querySelectorAll(".content-hallseats-row-seat");
+for (i = 0; i < bighall.length; i++) {
+    if (bighall[i].booked == true) {
+        hallseat[i].style.background = "red";
+    }
+}
+
+/* var buyButton = document.querySelector(".buy-button");
+buyButton.onclick = function() {
+  event.toElement.style.background = "red";
+}*/
+
+let bg = document.querySelectorAll('.content-hallseats-row-seat');
+for(i=0;i<bg.length;i++){
+if(bg[i].classList.contains('color')) {
+bg[i].style.background = 'red';
+}
+}
